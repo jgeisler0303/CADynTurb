11ddf4ebd76dbed7eaf56401451b775976a82983
Jens Geisler <jens.geisler@hs-flensburg.de>
2022-09-01 17:29:45 +0200

    fixed added script for mex simulation

--- matlab/set_path.m
+++ matlab/set_path.m
@@ -6,6 +6,8 @@ addpath(fullfile(base_dir, 'plot'))
 addpath(fullfile(base_dir, 'sim_data'))
 addpath(fullfile(base_dir, 'lin_model'))
 addpath(fullfile(base_dir, 'acados'))
+addpath(fullfile(base_dir, 'observer'))
+addpath(fullfile(base_dir, '../simulator'))
 
 addpath(fullfile(base_dir, '../../matlab-toolbox/FAST2MATLAB'))
 addpath(fullfile(base_dir, '../../matlab-toolbox/Utilities'))
