b96b41e53aa30f3b254f8f329c29794b48aa5741
Jens Geisler <jens.geisler@hs-flensburg.de>
2025-01-12 15:48:30 +0100

    activated tilt and cone in T1B1i model

--- model/T1B1i/testFAST2CADynTurb_T1B1i.m
+++ model/T1B1i/testFAST2CADynTurb_T1B1i.m
@@ -22,13 +22,26 @@ compileModel(model_name, model_dir, gen_dir, files_to_generate)
 
 %% compare stand-alone simulator with OpenFAST
 cd(gen_dir)
-fast_file= fullfile(model_dir, '../../ref_sim/sim_dyn_inflow/impulse_URef-12_maininput.fst');
-wind_dir= '';
+% fast_file= fullfile(model_dir, '../../ref_sim/sim_dyn_inflow/impulse_URef-12_maininput.fst');
+% wind_dir= '';
+% 
+% [~, base_file]= fileparts(fast_file);
+% sim_file= fullfile(gen_dir, [strrep(base_file, '_maininput', '') '.outb']);
+
+sim_dir= fullfile(model_dir, '../../ref_sim/sim_dyn_inflow');
+wind_dir= fullfile(model_dir, '../../ref_sim/wind');
+
+ref_sims= get_ref_sims(sim_dir, '1p1*_maininput.outb');
+v= 12;
+i= find(ref_sims.vv==12 & ref_sims.yaw==0)';
+fast_file= strrep(ref_sims.files{i}, '.outb', '.fst');
 
 [~, base_file]= fileparts(fast_file);
 sim_file= fullfile(gen_dir, [strrep(base_file, '_maininput', '') '.outb']);
+
 d_sim= sim_standalone(fullfile(gen_dir, ['sim_' model_name]), fast_file, sim_file, '-a 0.99');
 
-d_FAST= loadData(strrep(fast_file, '.fst', '.outb'), wind_dir);
+d_FAST= loadData(strrep(fast_file, '.fst', '.outb'), wind_dir, 0);
 
-plot_timeseries_cmp(d_FAST, d_sim, {'RtVAvgxh', 'BlPitchC1', 'LSSTipPxa', 'YawBrTDxp', {'RootMxb1' 'RootMxb2' 'RootMxb3'}, {'RootMyb1' 'RootMyb2' 'RootMyb3'}})
+% plot_timeseries_cmp(d_FAST, d_sim, {'RtVAvgxh', 'BlPitchC1', 'LSSTipPxa', 'YawBrTDxp', {'RootMxb1' 'RootMxb2' 'RootMxb3'}, {'RootMyb1' 'RootMyb2' 'RootMyb3'}})
+plot_timeseries_cmp(d_FAST, d_sim, {'RtVAvgxh', 'RootMxb1' 'RootMxb2' 'RootMxb3', 'RootMyb1' 'RootMyb2' 'RootMyb3'})
