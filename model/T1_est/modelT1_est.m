bf812e686b6c5630d0df76f5a356d0ec172da895
Jens Geisler <jens.geisler@hs-flensburg.de>
2025-12-28 12:23:19 +0100

    added first estimator model and made auxilliary states work

---
+++ model/T1_est/modelT1_est.m
@@ -0,0 +1,77 @@
+function T1_est = modelT1_est(params)
+% Simulation of a simplified horizontal axis wind turbine
+
+T1_est = MultiBodySystem('T1_est', {'tow_fa' 'phi_rot'}, {'dvwind' 'Tgen' 'theta'});
+
+T1_est.addAuxState('vwind')
+T1_est.addAuxImplODE(T1_est.getTimeDeriv(T1_est.aux_state.vwind)-T1_est.inputs.dvwind)
+
+T1_est.addParameter(params);
+T1_est.addExternal('cm');
+T1_est.addExternal('ct');
+T1_est.addExternal('theta_deg');
+T1_est.addExternal('lam');
+T1_est.addExternal('Trot', [T1_est.dof.tow_fa_d, T1_est.dof.phi_rot_d, T1_est.aux_state.vwind, T1_est.inputs.theta]);
+T1_est.addExternal('Fthrust', [T1_est.dof.tow_fa_d, T1_est.dof.phi_rot_d, T1_est.aux_state.vwind, T1_est.inputs.theta]);
+% TODO fix vector parameters
+T1_est.addExternalParameter('cm_lut', [], params.cm_lut);
+T1_est.addExternalParameter('ct_lut', [], params.ct_lut);
+T1_est.addExternalParameter('Arot', [], params.Arot);
+T1_est.addExternalParameter('Rrot', [], params.Rrot);
+T1_est.addExternalParameter('lambdaMax', [], params.lambdaMax);
+T1_est.addExternalParameter('lambdaMin', [], params.lambdaMin);
+T1_est.addExternalParameter('lambdaStep', [], params.lambdaStep);
+T1_est.addExternalParameter('rho', [], params.rho);
+T1_est.addExternalParameter('thetaMax', [], params.thetaMax);
+T1_est.addExternalParameter('thetaMin', [], params.thetaMin);
+T1_est.addExternalParameter('thetaStep', [], params.thetaStep);
+
+T1_est.addOutput('tow_fa_acc', T1_est.dof.tow_fa_dd);
+T1_est.addOutput('gen_speed', T1_est.dof.phi_rot_d*T1_est.params.GBRatio);
+
+T1_est.gravity(3) = -T1_est.params.g;
+
+% Tower
+tw_SID = SID(params.tw_sid, -1e-6, 'tower', T1_est);
+tower= ElasticBody(T1_est.dof.tow_fa, tw_SID, 'tower');
+T1_est.addChild(tower)
+
+% Nacelle
+nacelle = RigidBody('nacelle', [], T1_est.params.NacMass, diag([T1_est.params.NacXIner, T1_est.params.NacYIner, T1_est.params.NacZIner]));
+nacelle.translate([T1_est.params.NacCMxn, T1_est.params.NacCMyn, T1_est.params.NacCMzn])
+tower.addChild(nacelle)
+
+% Hub
+hub = RigidBody('hub', [], T1_est.params.HubMass, diag([T1_est.params.HubIner, T1_est.params.HubIner, T1_est.params.HubIner]));
+hub.translate([T1_est.params.HubCM+T1_est.params.OverHang-T1_est.params.NacCMxn, -T1_est.params.NacCMyn, T1_est.params.Twr2Shft-T1_est.params.NacCMzn]);
+hub.rotateLocalAxis('x', T1_est.dof.phi_rot)
+nacelle.addChild(hub);
+
+% Blades
+for i = 1:3
+    blade = RigidBody(sprintf('blade%d', i), [], T1_est.params.blade_mass, diag([T1_est.params.blade_I0_1_1, T1_est.params.blade_I0_2_2, T1_est.params.blade_I0_3_3]));
+    blade.translate([-T1_est.params.HubCM, 0, 0]);
+    % sym(2) is important to be able to simplify equations
+    blade.rotateLocalAxis('x', (i-1)*sym(2)/3*sym(pi))
+    blade.rotateLocalAxis('z', T1_est.inputs.theta)
+    blade.translate([0, 0, T1_est.params.blade_md0_3_1/T1_est.params.blade_mass]);
+    hub.addChild(blade)
+end
+
+
+% Geno (effectively fixed to hub but with gear ratio)
+geno = RigidBody('generator', [], 0, diag([T1_est.params.GenIner, 0, 0]));
+geno.rotateLocalAxis('x', T1_est.dof.phi_rot*T1_est.params.GBRatio)
+nacelle.addChild(geno)
+
+% at least for forces given in local coordinates, the kinematic modeling
+% must be completed
+T1_est.completeSetup()
+
+% Applied forces and moments
+hub.applyForce([T1_est.externals.Fthrust, 0, 0])
+hub.applyMoment([T1_est.externals.Trot, 0, 0])
+geno.applyMoment([-T1_est.inputs.Tgen, 0, 0])
+
+
+
--- model/T1_est/testFAST2CADynTurb_T1_est.m
+++ model/T1_est/testFAST2CADynTurb_T1_est.m
@@ -8,6 +8,7 @@ fst_file= fullfile(CADynTurb_dir, '5MW_Baseline/5MW_Land_DLL_WTurb.fst');
 
 model_name= 'T1_est';
 gen_dir= fullfile(model_dir, 'generated');
+gen_dir_m = [gen_dir '_M'];
 
 files_to_generate= {'_direct.hpp', '_param.hpp', 'model_indices.m', 'model_parameters.m'};
 
@@ -20,8 +21,33 @@ else
     save('params', 'param', 'tw_sid', 'bd_sid')
 end
 
+%% CADynM model
+% If you want to use this experimental feature, please clone
+% https://github.com/jgeisler0303/CADynM in a directory parallel to CADynTurb
+clc
+cd(model_dir)
+
+param.tw_sid= tw_sid;
+T1_est = modelT1_est(param);
+eom = T1_est.getEOM;
+T1_est.removeUnusedParameters();
+
+[~, ~] = mkdir(gen_dir_m);
+copyfile(fullfile(model_dir, [model_name '_Externals.hpp']), gen_dir_m)
+matlabTemplateEngine(fullfile(gen_dir_m, 'model_parameters.m'), 'model_parameters.m.mte', T1_est)
+matlabTemplateEngine(fullfile(gen_dir_m, 'model_indices.m'), 'model_indices.m.mte', T1_est)
+matlabTemplateEngine(fullfile(gen_dir_m, 'T1_est_param.hpp'), 'param.hpp.mte', T1_est)
+matlabTemplateEngine(fullfile(gen_dir_m, 'T1_est_direct.hpp'), 'direct.hpp.mte', T1_est)
+
+%% Build mex-file for CADynM generated model
+cd(gen_dir_m)
+clc
+makeCADynMex(model_name, '.', '', '', fullfile(CADynTurb_dir, 'simulator'))
+makeCADynEKFMex(model_name, model_dir, gen_dir_m)
+
 %% generate and compile all source code
-genCode([model_name '.mac'], gen_dir, files_to_generate, param, tw_sid, bd_sid);
+cd(model_dir)
+genCode([model_name '.mac'], gen_dir, files_to_generate, param, tw_sid, bd_sid, [0 1]); % no reduce const but const matrix elements only once
 writeModelParams(param, gen_dir);
 makeCADynMex(model_name, gen_dir, '', '', fullfile(CADynTurb_dir, 'simulator'))
 
@@ -36,7 +62,7 @@ wind_dir= fullfile(CADynTurb_dir, 'ref_sim/wind');
 ref_sims= get_ref_sims(sim_dir, '1p1*_maininput.outb');
 
 %% run Kalman filter
-cd(gen_dir)
+cd(gen_dir_m)
 model_indices
 ekf_config= T1_est_ekf_config;
 ix_vwind= find(ekf_config.estimated_states==vwind_idx);
@@ -53,4 +79,16 @@ for  i= find(ref_sims.vv==12 & ref_sims.yaw==0)'
     [d_est2, ~, ~, ~, ~, ~, Q, R]= run_simulation(model_name, d_in, param, [], 0, 2, Q, R);
 
     plot_timeseries_multi({d_in, d_est1, d_est2}, {'RtVAvgxh', 'BlPitchC', 'LSSTipVxa', 'GenTq', 'YawBrTDxp'})
+end
+
+%% compare mex simulations
+v= 11;
+for  i= find(ref_sims.vv==v & ref_sims.yaw==0)'
+    d_FAST= loadData(ref_sims.files{i}, wind_dir);
+
+    cd(gen_dir)
+    d_sim= run_simulation(model_name, d_FAST, param);
+    cd(gen_dir_m)
+    d_sim_M= run_simulation(model_name, d_FAST, param);
+    plot_timeseries_multi({d_FAST, d_sim, d_sim_M}, {'RtVAvgxh', 'BlPitchC', 'GenTq', 'LSSTipVxa', 'YawBrTDxp'}, {'FAST', 'CADyn', 'CADynM'});
 end
