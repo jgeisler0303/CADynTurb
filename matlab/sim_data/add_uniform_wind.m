28835c4e2489f5ffaead3d1c0c94cc3724275e92
Jens Geisler <jens.geisler@hs-flensburg.de>
2026-07-03 15:12:39 +0200

    fixed add_uniform_wind when wind lut has duplicate values

--- matlab/sim_data/add_uniform_wind.m
+++ matlab/sim_data/add_uniform_wind.m
@@ -2,26 +2,27 @@ function d = add_uniform_wind(d, file_path)
 
 uni_wind_file = strrep(file_path, '_maininput.outb', '_uni_wind.wnd');
 wind_tab = readUniformWind(uni_wind_file);
+[wind_time, idx] = unique(wind_tab.Time);
 
 time= d.Time;
 
 RtHSAvg= timeseries('RAWS');
 RtHSAvg.Time= d.Time;
-RtHSAvg.Data= interp1(wind_tab.Time, wind_tab.WindSpeed, time);
+RtHSAvg.Data= interp1(wind_time, wind_tab.WindSpeed(idx), time);
 RtHSAvg.DataInfo.Units= 'm/s';
 RtHSAvg.TimeInfo.Units= 's';
 d= d.addts(RtHSAvg);
 
 RtHSAvg= timeseries('RtHSAvg');
 RtHSAvg.Time= d.Time;
-RtHSAvg.Data= interp1(wind_tab.Time, wind_tab.HorizShear, time);
+RtHSAvg.Data= interp1(wind_time, wind_tab.HorizShear(idx), time);
 RtHSAvg.DataInfo.Units= 'm/s/m';
 RtHSAvg.TimeInfo.Units= 's';
 d= d.addts(RtHSAvg);
 
 RtVSAvg= timeseries('VertShear');
 RtVSAvg.Time= d.Time;
-RtVSAvg.Data= interp1(wind_tab.Time, wind_tab.WindSpeed, time);
+RtVSAvg.Data= interp1(wind_time, wind_tab.VertShear(idx), time);
 RtVSAvg.DataInfo.Units= 'm/s/m';
 RtVSAvg.TimeInfo.Units= 's';
 d= d.addts(RtVSAvg);      
