813c28ee30606737853c9e20bcb4e27e2ff79ff8
Jens Geisler <jens.geisler@hs-flensburg.de>
2026-07-03 15:42:29 +0200

    extended standalone simulation to MPC DISCON controllers

--- matlab/sim_data/sim_standalone.m
+++ matlab/sim_data/sim_standalone.m
@@ -1,9 +1,35 @@
-function d1= sim_standalone(model_path, fast_config, out_file, options, win_on_linux)
+function d1= sim_standalone(model_path, fast_config, out_file, options, win_on_linux, DISCON_dll, DLL_InFile)
 if ~exist('options', 'var')
     options= '';
 end
 if isunix && isempty(fileparts(model_path))
     model_path= fullfile('.', model_path);
+end
+
+if exist('DISCON_dll', 'var')
+    % read current configuration
+    fst_dir= fileparts(fast_config);
+    fstConfig = FAST2Matlab(fast_config);
+    ServoFileFile= strrep(GetFASTPar(fstConfig, 'ServoFile'), '"', '');
+    servoConfig = FAST2Matlab(fullfile(fst_dir, ServoFileFile));
+
+    % change DISCON
+    servoConfig = SetFASTPar(servoConfig, 'DLL_FileName', DISCON_dll);
+    if exist('DLL_InFile')
+        servoConfig = SetFASTPar(servoConfig, 'DLL_InFile', DLL_InFile);
+    end
+
+    % write ServoFile to temporary file
+    [~, ~, ext] = fileparts(ServoFileFile);
+    [~, DISCON_file] = fileparts(DISCON_dll);
+    MPC_ID = strrep(DISCON_file, 'DISCON_', '');
+    ServoFileFile_mpc = strrep(fullfile(fst_dir, ServoFileFile), ext, [MPC_ID ext]);
+    Matlab2FAST(servoConfig, fullfile(fst_dir, ServoFileFile), ServoFileFile_mpc);
+
+    fstConfig = SetFASTPar(fstConfig, 'ServoFile', ServoFileFile_mpc);
+    fast_config_mpc = strrep(fast_config, '.fst', [MPC_ID '.fst']);
+    Matlab2FAST(fstConfig, fast_config, fast_config_mpc);
+    fast_config = fast_config_mpc;
 end
 
 if ~exist('out_file', 'var') || isempty(out_file)
