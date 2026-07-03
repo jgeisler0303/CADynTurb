8d852d992719f2390438084aa4cd02f700776b30
Jens Geisler <jens.geisler@hs-flensburg.de>
2026-07-03 15:40:01 +0200

    added funtion to debug acados model equations

--- matlab/acados/eval_acados_model.m
+++ matlab/acados/eval_acados_model.m
@@ -1,6 +1,19 @@
-function [f, y] = eval_acados_model(model, x, xdot, u, ap)
+function [f, y, extra_val] = eval_acados_model(model, x, xdot, u, ap, extra)
+N = size(x, 2);
+ap = repmat(ap, 1, N);
+
 f_impl_expr=casadi.Function('f_impl_expr', {model.x, model.xdot, model.u, model.p}, {model.f_impl_expr});
-f= full(f_impl_expr(x, xdot, u, ap));
+f_impl_expr_map = f_impl_expr.map(N);
+
+f= full(f_impl_expr_map(x, xdot, u, ap));
 
 cost_y_expr=casadi.Function('cost_y_expr', {model.x, model.u, model.p}, {model.cost_y_expr});
-y= full(cost_y_expr(x, u, ap));
+cost_y_expr_map = cost_y_expr.map(N);
+y= full(cost_y_expr_map(x, u, ap));
+
+fn = fieldnames(extra);
+for i = 1:length(fn)
+    extra_fun=casadi.Function(fn{i}, {model.x, model.u, model.p}, {extra.(fn{i})});
+    extra_fun_map = extra_fun.map(N);
+    extra_val.(fn{i}) = full(extra_fun_map(x, u, ap));
+end
